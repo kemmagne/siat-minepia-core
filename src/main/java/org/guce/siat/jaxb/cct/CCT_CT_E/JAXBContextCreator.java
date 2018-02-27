17b57a8dc26c9801c63d27abe36957ab410bcd40
Hyacinthe TADZOTSA <romaulad.tadzotsa@guichetunique.org>
2018-02-27 20:19:39 +0100

    mise à jour xsd pour cte création d'un méthode supp pour l'obtention des flows

--- src/main/java/org/guce/siat/jaxb/cct/CCT_CT_E/JAXBContextCreator.java
+++ src/main/java/org/guce/siat/jaxb/cct/CCT_CT_E/JAXBContextCreator.java
@@ -8,44 +8,42 @@ import javax.xml.bind.JAXBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * The Class JAXBContextCreator.
  */
-public final class JAXBContextCreator
-{
-	/** The Constant LOG. */
-	private static final Logger LOG = LoggerFactory.getLogger(JAXBContextCreator.class);
+public final class JAXBContextCreator {
 
-	/** The instance. */
-	private static JAXBContext instance;
+    /**
+     * The Constant LOG.
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(JAXBContextCreator.class);
 
-	/**
-	 * Instantiates a new JAXB context creator.
-	 */
-	private JAXBContextCreator()
-	{
-	}
+    /**
+     * The instance.
+     */
+    private static JAXBContext instance;
 
-	/**
-	 * Gets the single instance of JAXBContextCreator.
-	 *
-	 * @return single instance of JAXBContextCreator
-	 */
-	public static synchronized JAXBContext getInstance()
-	{
-		try
-		{
-			if (instance == null)
-			{
-				instance = JAXBContext.newInstance(DOCUMENT.class);
-			}
-		}
-		catch (final JAXBException e)
-		{
-			LOG.error(Objects.toString(e), e);
-		}
-		return instance;
-	}
+    /**
+     * Instantiates a new JAXB context creator.
+     */
+    private JAXBContextCreator() {
+    }
+
+    /**
+     * Gets the single instance of JAXBContextCreator.
+     *
+     * @return single instance of JAXBContextCreator
+     */
+    public static synchronized JAXBContext getInstance() {
+        try {
+            if (instance == null) {
+                instance = JAXBContext.newInstance(DOCUMENT.class);
+            }
+        } catch (final JAXBException e) {
+            LOG.error(Objects.toString(e), e);
+        }
+        return instance;
+    }
 
 }
+
