7c23772a2e521844195b2182eafb8ad411cb6c44
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-08-05 07:49:33 +0100

    centralize TREATMENT_STEPS_CODES on MinaderStatisticsService interface

--- src/main/java/org/guce/siat/core/ct/service/MinaderStatisticsService.java
+++ src/main/java/org/guce/siat/core/ct/service/MinaderStatisticsService.java
@@ -1,7 +1,9 @@
 package org.guce.siat.core.ct.service;
 
+import java.util.Arrays;
 import java.util.List;
 import org.guce.siat.common.model.User;
+import org.guce.siat.common.utils.enums.StepCode;
 import org.guce.siat.core.ct.filter.MinaderFileTrackingFilter;
 import org.guce.siat.core.ct.model.MinaderFileTracking;
 
@@ -10,6 +12,8 @@ import org.guce.siat.core.ct.model.MinaderFileTracking;
  * @author ht
  */
 public interface MinaderStatisticsService {
+
+    List<StepCode> TREATMENT_STEPS_CODES = Arrays.asList(StepCode.ST_CT_04, StepCode.ST_CT_48, StepCode.ST_CT_63, StepCode.ST_CT_55);
 
     List<User> retrievePotentialAgents(MinaderFileTracking fileTracking);
 
