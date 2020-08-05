815733771d53f7b931b023cd4be4ca910c92083a
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-08-05 07:48:46 +0100

    starting creation of MinaderFile entity class

---
+++ src/main/java/org/guce/siat/core/ct/model/MinaderFile.java
@@ -0,0 +1,17 @@
+package org.guce.siat.core.ct.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ht
+ */
+//@Entity
+//@Table(name = "MINADER_FILES")
+public class MinaderFile implements Serializable {
+
+//    @Id
+//    @Column(name = "ID")
+    private Long id;
+
+}
