0d712933d865c20a6c20cf2973e0aca943cd7152
Hyacinthe TADZOTSA <romuald.tadzotsa@guichetunique.org>
2021-04-12 15:10:02 +0100

    change implementation of methods findCDAs, findTreatmentSocieties and findDestinationCountries - find data directly in tables instead of MINADER_FILES view ; return List<Pair> instead of Map<String,String> in minader statistics dao and service

--- src/main/java/org/guce/siat/core/ct/service/MinaderStatisticsService.java
+++ src/main/java/org/guce/siat/core/ct/service/MinaderStatisticsService.java
@@ -2,8 +2,7 @@ package org.guce.siat.core.ct.service;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import org.guce.siat.common.model.Country;
+import org.guce.siat.common.model.Pair;
 import org.guce.siat.common.model.User;
 import org.guce.siat.common.utils.enums.StepCode;
 import org.guce.siat.core.ct.filter.MinaderFileTrackingFilter;
@@ -17,11 +16,11 @@ public interface MinaderStatisticsService {
 
     List<StepCode> TREATMENT_STEPS_CODES = Arrays.asList(StepCode.ST_CT_04, StepCode.ST_CT_48, StepCode.ST_CT_55, StepCode.ST_CT_63);
 
-    Map<String, String> findCDAs();
+    List<Pair> findCDAs();
 
-    Map<String, String> findTreatmentSocieties();
+    List<Pair> findTreatmentSocieties();
 
-    List<Country> findDestinationCountries();
+    List<Pair> findDestinationCountries();
 
     List<User> retrievePotentialAgents(MinaderFileTracking fileTracking);
 
