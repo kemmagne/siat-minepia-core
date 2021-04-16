3d9594f5db3c0ab6b4277b4335544c2dd7a165c0
Hyacinthe TADZOTSA <romuald.tadzotsa@guichetunique.org>
2021-04-16 15:56:08 +0100

    make sure almost one element is returned when needed in class InspectionReportDaoImpl ; change the way errors are logged

--- src/main/java/org/guce/siat/core/ct/dao/impl/InspectionReportDaoImpl.java
+++ src/main/java/org/guce/siat/core/ct/dao/impl/InspectionReportDaoImpl.java
@@ -2,9 +2,7 @@ package org.guce.siat.core.ct.dao.impl;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import javax.persistence.NoResultException;
-import javax.persistence.NonUniqueResultException;
 import javax.persistence.TypedQuery;
 import org.apache.commons.collections.CollectionUtils;
 import org.guce.siat.common.dao.impl.AbstractJpaDaoImpl;
@@ -12,8 +10,6 @@ import org.guce.siat.common.model.FileItem;
 import org.guce.siat.common.model.ItemFlow;
 import org.guce.siat.core.ct.dao.InspectionReportDao;
 import org.guce.siat.core.ct.model.InspectionReport;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,15 +22,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class InspectionReportDaoImpl extends AbstractJpaDaoImpl<InspectionReport> implements InspectionReportDao {
 
     /**
-     * The Constant LOG.
-     */
-    private static final Logger LOG = LoggerFactory.getLogger(InspectionReportDaoImpl.class);
-
-    /**
      * Instantiates a new inspection report dao impl.
      */
     public InspectionReportDaoImpl() {
-        super();
         setClasse(InspectionReport.class);
     }
 
@@ -44,11 +34,11 @@ public class InspectionReportDaoImpl extends AbstractJpaDaoImpl<InspectionReport
 	 * @see org.guce.siat.core.ct.dao.InspectionReportDao#findInspectionReportsByItemFlowList(java.util.List)
      */
     @Override
-    public List<InspectionReport> findInspectionReportsByItemFlowList(final List<ItemFlow> itemFlows) {
+    public List<InspectionReport> findInspectionReportsByItemFlowList(List<ItemFlow> itemFlows) {
         if (CollectionUtils.isNotEmpty(itemFlows)) {
-            final String hql = "SELECT a FROM InspectionReport a WHERE a.itemFlow IN (:itemFlows)";
+            String hql = "SELECT a FROM InspectionReport a WHERE a.itemFlow IN (:itemFlows)";
 
-            final TypedQuery<InspectionReport> query = super.entityManager.createQuery(hql, InspectionReport.class);
+            TypedQuery<InspectionReport> query = super.entityManager.createQuery(hql, InspectionReport.class);
             query.setParameter("itemFlows", itemFlows);
 
             return query.getResultList();
@@ -63,7 +53,7 @@ public class InspectionReportDaoImpl extends AbstractJpaDaoImpl<InspectionReport
 	 * @see org.guce.siat.core.ct.dao.InspectionReportDao#findByItemFlow(org.guce.siat.common.model.ItemFlow)
      */
     @Override
-    public InspectionReport findByItemFlow(final ItemFlow itemFlow) {
+    public InspectionReport findByItemFlow(ItemFlow itemFlow) {
 
         if (itemFlow == null) {
             return null;
@@ -71,35 +61,40 @@ public class InspectionReportDaoImpl extends AbstractJpaDaoImpl<InspectionReport
 
         try {
             TypedQuery<InspectionReport> query = super.entityManager.createQuery("SELECT a FROM InspectionReport a WHERE a.itemFlow.id = :itemFlowId ORDER BY a.itemFlow.id DESC", InspectionReport.class);
-
             query.setParameter("itemFlowId", itemFlow.getId());
+            query.setMaxResults(1);
 
             return query.getSingleResult();
-        } catch (final NoResultException | NonUniqueResultException e) {
-            LOG.info(Objects.toString(e), e);
+        } catch (NoResultException e) {
+            logger.info("No result in method org.​guce.​siat.​core.​ct.​model.InspectionReportDaoImpl.findByItemFlow - file number :  {} ; file item line number : {} ; item flow : {}", itemFlow.getFileItem().getFile().getNumeroDossier(), itemFlow.getFileItem().getLineNumber(), itemFlow.getFileItem().getId());
         }
 
         return null;
     }
 
     @Override
-    public InspectionReport findLastInspectionReportsByFileItem(final FileItem fileItem) {
-        if (fileItem != null) {
-            final StringBuilder hqlBuilder = new StringBuilder();
+    public InspectionReport findLastInspectionReportsByFileItem(FileItem fileItem) {
 
-            hqlBuilder.append("SELECT a FROM InspectionReport a ");
-            hqlBuilder.append("WHERE a.itemFlow.fileItem.id = :fileItemId ");
-            hqlBuilder.append("ORDER BY a.id DESC");
-
-            final TypedQuery<InspectionReport> query = super.entityManager.createQuery(hqlBuilder.toString(), InspectionReport.class);
-            query.setParameter("fileItemId", fileItem.getId());
-            query.setMaxResults(1);
-            try {
-                return query.getSingleResult();
-            } catch (final NoResultException | NonUniqueResultException e) {
-                LOG.info(Objects.toString(e), e);
-            }
+        if (fileItem == null) {
+            return null;
         }
+
+        StringBuilder hqlBuilder = new StringBuilder();
+
+        hqlBuilder.append("SELECT a FROM InspectionReport a ");
+        hqlBuilder.append("WHERE a.itemFlow.fileItem.id = :fileItemId ");
+        hqlBuilder.append("ORDER BY a.id DESC");
+
+        TypedQuery<InspectionReport> query = super.entityManager.createQuery(hqlBuilder.toString(), InspectionReport.class);
+        query.setParameter("fileItemId", fileItem.getId());
+        query.setMaxResults(1);
+
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            logger.info("No result in method org.​guce.​siat.​core.​ct.​model.InspectionReportDaoImpl.findLastInspectionReportsByFileItem - file number :  {} ; file item line number : {}", fileItem.getFile().getNumeroDossier(), fileItem.getLineNumber());
+        }
+
         return null;
     }
 
